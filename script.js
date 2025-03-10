07b42f38731c25ef0db8cecb5b131fc9fbc4156c
kamal2652159 <2652159@students.wits.ac.za>
2025-03-10 15:49:47 +0200

    New stuff

---
+++ script.js
@@ -0,0 +1,41 @@
+document.getElementById('btnSubmit').onclick = function() {
+    const countryName = document.getElementById('CountryName').value;
+    const countryInfo = document.getElementById('country-info');
+    const borderCountry = document.getElementById('bordering-countries');
+
+    // Clear previous information
+    countryInfo.innerHTML = '';
+    borderCountry.innerHTML = '';
+
+    // Fetch country data from the REST Countries API
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then(response => response.json())
+    .then(data => {
+        const country = data[0];
+        // Update the DOM with country information
+        countryInfo.innerHTML = `<h1>${country.name.common}</h1>
+                                 <p>Capital: ${country.capital ? country.capital[0] : 'No capital'}</p>
+                                 <p>Population: ${country.population.toLocaleString()}</p>
+                                 <p>Region: ${country.region}</p>
+                                 <img src="${country.flags.svg}" alt="Flag of ${country.name.common}" style="width: 150px;">`;
+
+        // Handle bordering countries
+        if (country.borders) {
+            borderCountry.innerHTML = '<h2>Neighbouring Countries:</h2>';
+            country.borders.forEach(border => {
+                fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+                .then(response => response.json())
+                .then(borderCountryData => {
+                    borderCountry.innerHTML += `<p>${borderCountryData[0].name.common}</p>
+                                                <img src="${borderCountryData[0].flags.svg}" alt="Flag of ${borderCountryData[0].name.common}" style="width: 100px;">`;
+                });
+            });
+        } else {
+            borderCountry.innerHTML += '<p>No bordering countries.</p>';
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+        countryInfo.innerHTML = '<p>Error loading country, your spelling could be wrong</p>';
+    });
+};
